44e179f69472b5ebc458ffcc93b628483316c7e7
Jihyun Song <jihyunsong42@gmail.com>
2020-09-18 01:31:09 -0230

    STATICROOT on settings.py

--- KSM/KSM/settings.py
+++ KSM/KSM/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -120,6 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static').replace('\\', '/')
 
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
